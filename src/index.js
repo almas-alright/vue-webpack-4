ca92711d8b74d07aff343d1e4cb8090b7e16988e
weDevs Macbook <wedevs-macbook-2@MacBook-Pro.localdomain>
2018-09-17 16:42:27 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
+
+
+console.log(`I'm a silly entry point`);
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
